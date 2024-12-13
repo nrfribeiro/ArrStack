83a5c08e16ec6e3e344f25e96e837b59b3700d37
Nuno Ribeiro <nribeiro@gmail.com>
2024-12-13 15:29:04 +0000

    .

--- dirs_create.sh
+++ dirs_create.sh
@@ -1,8 +1,8 @@
-export FOLDER_FOR_MEDIA=/your-media-folder       # Change to where you want your media to be stored
-export FOLDER_FOR_DATA=/your-app-configs         # Change to where you want your container configurations to be stored
+export FOLDER_FOR_MEDIA=/ArrStack/media     # Change to where you want your media to be stored
+export FOLDER_FOR_DATA=/ArrStack/data         # Change to where you want your container configurations to be stored
 
 export PUID=1000
-export PGID=1000
+export PGID=995
 
 mkdir -p $FOLDER_FOR_DATA/{authelia/assets,bazarr,ddns-updater,gluetun,heimdall,homarr/{configs,data,icons},homepage,jellyfin,jellyseerr,lidarr,mylar,plex,portainer,prowlarr,qbittorrent,radarr,readarr,sabnzbd,sonarr,swag,tdarr/{server,configs,logs},tdarr_transcode_cache,unpackerr,whisparr}
 mkdir -p $FOLDER_FOR_MEDIA/media/{anime,audio,books,comics,movies,music,photos,tv,xxx}
