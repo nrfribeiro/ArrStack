81d2fe13439dbb9572e3a0c693e41a7694fac1fe
Nuno Ribeiro <nribeiro@gmail.com>
2024-12-13 13:52:11 +0000

    cenas

---
+++ start.sh
@@ -0,0 +1,2 @@
+docker compose --file min-vpn_single-nr/docker-compose-mediastack.yaml --env-file min-vpn_single-nr/docker-compose.env up -d  
+docker network connect mediastack qbittorrent
---
+++ stop.sh
@@ -0,0 +1,1 @@
+docker compose --file min-vpn_single-nr/docker-compose-mediastack.yaml --env-file min-vpn_single-nr/docker-compose.env down
