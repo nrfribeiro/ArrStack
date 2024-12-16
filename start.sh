953cd4fa1ee993e29f4c214738b1a9d95393249c
Nuno Ribeiro <nribeiro@gmail.com>
2024-12-16 18:40:41 +0000

    .

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 docker compose --file min-vpn_single-nr/docker-compose-mediastack.yaml --env-file min-vpn_single-nr/docker-compose.env up -d  
-#docker network connect mediastack qbittorrent<
+docker network connect mediastack qbittorrent
+docker network disconnect mediastack_default qbittorrent
