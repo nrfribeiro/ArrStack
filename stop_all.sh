7311ae0bedaf7e4fd8b017ca4e2765bb2777c03a
Nuno Ribeiro <nribeiro@gmail.com>
2024-12-18 15:11:09 +0000

    f

--- stop_all.sh
+++ stop_all.sh
@@ -1,2 +1,2 @@
-docker compose --file min-vpn_single-nr/docker-compose-mediastack.yaml --env-file min-vpn_single-nr/docker-compose.env down
+docker compose --file arr/docker-compose-mediastack.yaml --env-file arr/docker-compose.env down
 docker compose --file watchtower/docker-compose.yaml  down 
